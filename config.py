411e617608329b18c249825393fe6bdbdbbbdd7e
littlesnoopy9-lab <shulzhenko.alina1992@gmail.com>
2026-06-10 17:46:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,4 @@ load_dotenv()
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "0"))
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "developer")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "0"))
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "developer")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
